0b41e243b0dfa2db7d3f2cc20f39241d0718250f
Paola Ayala <paolaaz235@gmail.com>
2022-03-26 22:47:24 -0400

    Get Bot to Reply in One Channel

--- index.ts
+++ index.ts
@@ -2,13 +2,17 @@ import DiscordJS, { Intents } from 'discord.js';
 import dotenv from 'dotenv';
 import cowsay from './utils/cowsay';
 
-//const output = cowsay();
-//console.log(output);
-
 dotenv.config();
 
-//const PREFIX = process.env.PREFIX;
-//console.log(PREFIX);
+const CHANNELS = process.env.CHANNELS || null;
+
+if (!CHANNELS) {
+  console.error('CHANNELS is not defined');
+  process.exit(1);
+}
+
+const channels = CHANNELS.split(',');
+console.table(channels);
 
 const client = new DiscordJS.Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
@@ -19,16 +23,17 @@ client.on('ready', () => {
 });
 
 client.on('messageCreate', (message) => {
+  if (!channels.includes(message.channel.id)) return;
   const PREFIX = process.env.PREFIX || 'paz!';
-  // console.log(PREFIX);
-  // console.log(PREFIX.length);
   const check = message.content;
   const bool = check.startsWith(PREFIX);
 
+  // Check if a message starts with PREFIX, if not, exit
   if (bool != true) {
     return;
   }
 
+  //Parse the messages
   const args = message.content
     .toLowerCase()
     .substring(PREFIX.length)
@@ -52,7 +57,7 @@ client.on('messageCreate', (message) => {
       .catch(console.error);
   }
   if (command === 'cowsay') {
-    //message.react('🐄').then(console.log).catch(console.error);
+    //Check for the user giving a proper image name
     try {
       cowsay(param);
     } catch (error) {
